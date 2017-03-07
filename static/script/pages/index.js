d851c32a961b992985b0f3844aa6a2310c642134
何冲 <2472604879@qq.com>
2017-03-07 09:56:17 +0800

    a new webapp
    reader for ficton

---
+++ static/script/pages/index.js
@@ -0,0 +1,8 @@
+/*
+* @Author: Administrator
+* @Date:   2017-03-06 16:00:41
+* @Last Modified by:   Administrator
+* @Last Modified time: 2017-03-06 16:00:48
+*/
+
+'use strict';
---
+++ static/test.js
@@ -0,0 +1,1 @@
+alert('my  koa-static try');
