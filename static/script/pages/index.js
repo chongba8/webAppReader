ba079c82737409ac62fc575c7e5148f1a439fadf
何冲 <2472604879@qq.com>
2017-03-09 21:51:38 +0800

    add new pages
    增加一个图书详情页和书架页面

--- static/script/pages/index.js
+++ static/script/pages/index.js
@@ -1,8 +1,33 @@
-/*
-* @Author: Administrator
-* @Date:   2017-03-06 16:00:41
-* @Last Modified by:   Administrator
-* @Last Modified time: 2017-03-06 16:00:48
-*/
-
-'use strict';
+$.get('/ajax/index',function(d){
+	new Vue({
+		el:'#app',
+		data:{
+			top:d.items[0].data.data,
+			hot:d.items[1].data.data,
+			recommend:d.items[2].data.data,
+			female:d.items[3].data.data,
+			male:d.items[4].data.data,
+			free:d.items[5].data.data,
+			topic:d.items[6].data.data,
+			position:0,
+			header_position:0,
+			tab_1_class:'Swipe-tab-on',
+			tab_2_calss:'',
+		},
+		methods:{
+			tabSwitch:function(pos){
+				if(pos==0){
+					this.position=0;
+					this.header_position=0;
+					this.tab_1_class='Swipe-tab-on';
+					this.tab_2_calss='';
+				}else{
+					this.position=-550;
+					this.header_position=220;
+					this.tab_2_class='Swipe-tab-on';
+					this.tab_1_calss='';
+				}
+			}
+		}
+	})
+},'json')
